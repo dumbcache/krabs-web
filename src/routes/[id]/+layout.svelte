827e3d593eef101ca655885d930b1b0a240f207f
dumbcache <dumbcache@gmail.com>
2023-05-14 21:25:25 +0530

    css changes to preview component

--- src/routes/[id]/+layout.svelte
+++ src/routes/[id]/+layout.svelte
@@ -53,6 +53,9 @@
         .main :global(.drop) {
             position: fixed;
             top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
             border: none;
         }
     }
